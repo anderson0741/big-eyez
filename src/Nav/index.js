dceff0d1c59742d07c8c5457706bcd7fcf70bfe5
Lawrence Anderson <anderson0741@icloud.com>
2018-05-17 03:43:52 -0600

    instagram icon

--- src/Nav/index.js
+++ src/Nav/index.js
@@ -6,11 +6,12 @@ import { connect } from "react-redux";
 import { logout } from "../redux/auth";
 import Logo from '../images/Logo1.png';
 import styled from 'styled-components';
+import instagram from '../images/instagram1.png';
 
 import './Nav.css';
 import { HashLink as Link } from 'react-router-hash-link';
 
-const StyledLink = styled(Link)`
+const StyledLink = styled(Link) `
     color: white;
     font-size: 30px;
     text-decoration: none;
@@ -39,6 +40,12 @@ class Nav extends Component {
                                 <StyledLink smooth="true" className='links' to="/#home">Home</StyledLink>
                                 <StyledLink smooth="true" className='links' to="/#about">About</StyledLink>
                                 <StyledLink smooth="true" className='links' to="/#contact">Contact</StyledLink>
+                                <a className="social" href="https://www.instagram.com/marys.esthetics/?hl=en">
+                                    <div className="social_icon">
+                                        <img src={instagram} />
+                                        <h1>Instagram</h1>
+                                    </div>
+                                </a>
                             </div>
                             {isAuthenticated ? <div className="navBod_nav user">
                                 <button className="dropbtn">Username</button>
@@ -51,6 +58,7 @@ class Nav extends Component {
                     </div>
                 </div>
                 <div className="functionalDiv">
+                    {/* <h1 className="smallBig">Big Eyes Esthetics</h1> */}
                     <nav role="navigation">
                         <div className="menuToggle">
                             <input type="checkbox" />
@@ -63,6 +71,12 @@ class Nav extends Component {
                                 {/* <StyledLink smooth="true" className="navLink" to="/#about"><li>About</li></a> */}
                                 <StyledLink smooth="true" className="navLink" to="/#about"><li>About</li></StyledLink>
                                 <StyledLink smooth="true" className="navLink" to="/#contact"><li>Contact Us</li></StyledLink>
+                                <a className="social" href="https://www.instagram.com/marys.esthetics/?hl=en">
+                                    <div className="social_icon">
+                                        <img src={instagram} />
+                                        <h1>Instagram</h1>
+                                    </div>
+                                </a>
                             </ul>
                         </div>
                     </nav>
